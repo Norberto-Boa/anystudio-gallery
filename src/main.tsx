a3f1d7582c9c36e3bf5bb9ff0338f1a15a0d6204
Norberto Boa <nboa.26@gmail.com>
2026-01-07 00:19:49 +0200

    ADD: Add web analytics to the website

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { SpeedInsights } from '@vercel/speed-insights/react'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <SpeedInsights />
+    <Analytics />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
